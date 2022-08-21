1397fdd042a1a360f82e396a8a1d1e63fb9949d9
Nikita Shulga <nshulga@fb.com>
2022-08-21 17:01:40 +0000

    [CMake] Add functorch target
    Add libfunctorch target as PyTorch cmake build system
    
    ghstack-source-id: 56f3a6043169f285dcbca308cad55951222492f3
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/83464

--- setup.py
+++ setup.py
@@ -614,6 +614,19 @@ class build_ext(setuptools.command.build_ext.build_ext):
                     os.makedirs(dst_dir)
                 self.copy_file(src, dst)
                 i += 1
+
+        # Copy functorch extension
+        for i, ext in enumerate(self.extensions):
+            if ext.name != "functorch._C":
+                continue
+            fullname = self.get_ext_fullname(ext.name)
+            filename = self.get_ext_filename(fullname)
+            fileext = os.path.splitext(filename)[1]
+            src = os.path.join(os.path.dirname(filename), "functorch" + fileext)
+            if os.path.exists(src):
+                report("Copying {} from {} to {}".format(ext.name, src, filename))
+                self.copy_file(src, filename)
+                del self.extensions[i]
         setuptools.command.build_ext.build_ext.build_extensions(self)
 
 
@@ -899,6 +912,12 @@ def configure_extension_build():
                     name=str('caffe2.python.caffe2_pybind11_state_hip'),
                     sources=[]),
             )
+    if cmake_cache_vars['BUILD_FUNCTORCH']:
+        extensions.append(
+            Extension(
+                name=str('functorch._C'),
+                sources=[]),
+        )
 
     cmdclass = {
         'bdist_wheel': wheel_concatenate,
